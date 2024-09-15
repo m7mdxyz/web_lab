b97afed27ab82c33d732560e008354c678500b88
m7mdxyz <mouhmad11@gmail.com>
2024-09-15 12:37:16 +0300

    Done from Lab 4.

--- Django/libraryproject/apps/usermodule/views.py
+++ Django/libraryproject/apps/usermodule/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    name = request.GET.get("name") or "world!"
+    return render(request, "bookmodule/index.html", {"name": name})
+
+def index2(request, val1 = 0): #add the view function (index2)
+ return HttpResponse("value1 = "+str(val1))
